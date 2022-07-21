2f39045a69fd6ae432edfe8c21d1738c69921bbe
vedantisawarkar <99338092+vedantisawarkar@users.noreply.github.com>
2022-07-21 08:36:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function myjs(){
+    alert("Product Added to the Cart");
+    document.getElementById("main").innerHTML='output';
+    console.log("Product Added to the Cart");
+    var a=4;
+    var b=0;
+    var c=a+b;
+    alert(c);
+    console.log(c);
+}
+
+function removejs(){
+    alert("Product Remove from the Cart");
+    document.getElementById("main").innerHTML='output';
+    console.log("Product Remove from the Cart");
+    var a=4;
+    var b=1;
+    var c=a-b;
+    alert(c);
+    console.log(c);
+}
